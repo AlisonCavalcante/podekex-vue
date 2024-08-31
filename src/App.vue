7cae2e02e36ee8e72ee57dd1ad95744fd31f22a2
Alison Cavalcante <alison.rscavalcante@gmail.com>
2024-08-31 16:45:53 -0300

    feat: listagem inicial dos cards dos pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
-  <div id="podekex">
+  <div id="pokedex">
     <h1 class="text--yellow">Pokedex</h1>
+    <div id="container">
+      <PokemonCard
+       v-for="(pokemon, index) in pokemons"
+       :key="index" 
+       :pokemon="pokemon" 
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import PokemonCard from './components/PokemonCard.vue';
+
 
 export default {
   name: 'App',
+  
+  components: {
+    PokemonCard
+  },
+
+  data() {
+    return {
+      pokemons: []
+    }
+  },
+
+  mounted() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=50&offset=50").then((response) => {
+      this.pokemons = response.data.results;
+    })
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-#pokedex {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  max-width: 1000px;
-  margin: 0 auto;
-  padding: 24px;
-}
+
 </style>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="pokemon-card">
+        <div class="pokemon-container-card">
+            <span class="pokemon-name">
+                {{ get_name(pokemon) }}
+            </span>
+            <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
+                pokemon
+            )}.png`" :alt="pokemon.name" width="50%">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PokemonCard',
+    props: {
+        pokemon: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        get_id(pokemon) {
+            return Number(pokemon.url.split("/")[6]);
+        },
+        get_name(pokemon) {
+            return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
