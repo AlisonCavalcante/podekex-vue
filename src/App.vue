bb46a3d43a526371aa296d0e46e496dd9124c86c
Alison Cavalcante <alison.rscavalcante@gmail.com>
2024-09-01 11:34:09 -0300

    feat: input de pesquisa inicial

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="pokedex">
     <h1 class="text--yellow">Pokedex</h1>
+    <input 
+    type="text"
+    class="search-input"
+    placeholder="Charmander"
+    v-model="searchQuery"
+    >
     <div id="container">
       <PokemonCard
-       v-for="(pokemon, index) in pokemons"
+       v-for="(pokemon, index) in filtered_pokemons"
        :key="index" 
        :pokemon="pokemon" 
       />
@@ -25,7 +31,8 @@ export default {
 
   data() {
     return {
-      pokemons: []
+      pokemons: [],
+      searchQuery: '',
     }
   },
 
@@ -33,7 +40,15 @@ export default {
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=50&offset=50").then((response) => {
       this.pokemons = response.data.results;
     })
-  }
+  },
+ 
+  computed: {
+    filtered_pokemons() {
+      return this.pokemons.filter((item) => {
+        return item.name.includes(this.searchQuery.toLowerCase());
+      })
+    }
+  },
 };
 </script>
 
