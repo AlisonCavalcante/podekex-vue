133ce827bcd37e953f52e3d0c2e661c51f9adf89
Alison Cavalcante <alison.rscavalcante@gmail.com>
2024-09-03 22:27:42 -0300

    chore: instal bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 import './styles/main.scss';
+
+
 
 Vue.config.productionTip = false;
 
